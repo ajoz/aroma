586a6f8ae4519319823a0ef4a0877564749beeac
Andrzej Jozwiak <andrzej.jozwiak@tomtom.com>
2018-11-02 15:14:38 +0100

    XML resources for tests

--- aroma-core/src/test/java/io/aroma/core/AromaTest.java
+++ aroma-core/src/test/java/io/aroma/core/AromaTest.java
@@ -17,6 +17,31 @@ import java.util.Map;
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE)
 public class AromaTest {
+    /*
+    Tests:
+    -- Non errors:
+    X ---- map tag exists, there are zero entries -> Empty map, default type
+    X ---- map tag exists, there is one entry -> Singleton map, default type, default collection
+    X ---- map tag exists, there are multiple entries -> Full map, default type, default collection
+    X ---- map tag exists, type is set, there are zero entries ->
+    X ---- map tag exists, type is set, there is one entry
+    X ---- map tag exists, type is set, there are multiple entries
+    X ---- map tag exists, collection is set, there are zero entries
+    X ---- map tag exists, collection is set, there is one entry
+    X ---- map tag exists, collection is set, there are multiple entries
+    X ---- map tag exists, both attrs set, there are zero entries
+    X ---- map tag exists, both attrs set, there is one entry
+    X ---- map tag exists, both attrs set, there are multiple entries
+    -- Errors:
+    X ---- missing map tag -> Empty map, default type
+    X ---- misplaced map tag
+    X ---- unknown map tag attributes
+    X ---- unknown child tags
+    ---- unknown entry tag attributes
+    ---- converter error, key conversion error
+    X ---- converter error, value conversion error
+    */
+
     @Test
     public void test() {
         // given:
@@ -35,4 +60,6 @@ public class AromaTest {
         // then:
         Assert.assertTrue(parsedMap.isEmpty());
     }
+
+
 }
